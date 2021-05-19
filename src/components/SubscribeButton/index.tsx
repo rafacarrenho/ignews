c02c7f908f9e820390979552903d497043cdce84
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-18 22:35:48 -0300

    Feat: Add stripe and faunaDB

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -5,8 +8,29 @@ type SubscribeButtonProps = {
 };
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJS();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
